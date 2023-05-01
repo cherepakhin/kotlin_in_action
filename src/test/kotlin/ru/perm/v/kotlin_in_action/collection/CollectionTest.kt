e05a6c52a9d5758e06fb901bb366191f0211bb4e
Черепахин Василий <vasi.che@gmail.com>
2023-05-01 19:29:39 +0500

    Collection.maxBy

--- src/test/kotlin/ru/perm/v/kotlin_in_action/collection/CollectionTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/collection/CollectionTest.kt
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CollectionTest {
-    data class Person(val name: String, val age: Int)
-    val persons = listOf(Person("NAME1", 1), Person("NAME2", 2))
+    data class Person(val id:Int,val name: String, val age: Int)
+    val persons = listOf(Person(1,"NAME1", 10), Person(2, "NAME2", 20))
 
     @Test
     internal fun collectionSizeTest() {
         Assertions.assertEquals(2, persons.size)
     }
 
+    @Test
+    internal fun maxByTest() {
+        Assertions.assertEquals(20, persons.maxBy { it.age?:0 }.age)
+    }
 
 }
