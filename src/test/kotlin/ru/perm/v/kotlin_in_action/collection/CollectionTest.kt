b948996ee094b7bd7fd7904d592403d725674443
Черепахин Василий <vasi.che@gmail.com>
2023-05-01 19:36:06 +0500

    elvis

--- src/test/kotlin/ru/perm/v/kotlin_in_action/collection/CollectionTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/collection/CollectionTest.kt
@@ -4,17 +4,21 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class CollectionTest {
-    data class Person(val id:Int,val name: String, val age: Int)
-    val persons = listOf(Person(1,"NAME1", 10), Person(2, "NAME2", 20))
+    data class Person(val id:Int,val name: String, val age: Int?)
+    val persons = listOf(
+        Person(1,"NAME1", 10),
+        Person(2, "NAME2", 20),
+        Person(-1, "NAME-1", null))
 
     @Test
     internal fun collectionSizeTest() {
-        Assertions.assertEquals(2, persons.size)
+        Assertions.assertEquals(3, persons.size)
     }
 
     @Test
-    internal fun maxByTest() {
-        Assertions.assertEquals(20, persons.maxBy { it.age?:0 }.age)
+    // elvis. Если age=null, то -100
+    internal fun maxByAgeTestWithElvis() {
+        Assertions.assertEquals(20, persons.maxBy { it.age ?:-100 }.age)
     }
 
 }
