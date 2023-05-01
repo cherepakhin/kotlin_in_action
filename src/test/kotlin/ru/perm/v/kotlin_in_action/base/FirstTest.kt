84bf42c3c54391106bb1469cb1ed906d4527fc4f
Черепахин Василий <vasi.che@gmail.com>
2023-05-01 20:04:04 +0500

    createWithDefaultIfNull

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/FirstTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/FirstTest.kt
@@ -5,21 +5,35 @@ import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class FirstTest {
-    // "?" м.б. null
-    data class Person(val name: String, val age: Int?)
 
     @Test
     internal fun create() {
+        data class Person(val name: String, val age: Int)
+
         val person = Person("NAME", 100)
         assertEquals(100, person.age)
         assertEquals("NAME", person.name)
     }
+
     @Test
     internal fun createWithNull() {
-        // val age: Int? м.б. null
+        // "?" м.б. null
+        data class Person(val name: String, val age: Int?)
+        // val age: Int? м.б. null. В конструкторе указано, что age нужно определить и можно null
         val person = Person("NAME", null)
         assertNull(person.age)
         assertEquals("NAME", person.name)
     }
 
+    @Test
+    internal fun createWithDefaultIfNull() {
+        // "?" м.б. null
+        data class Person(val name: String, val age: Int? = 10)
+        // val age: Int? м.б. null. В конструкторе указано, что age при null установить 10
+        val person = Person("NAME")
+        assertEquals(10, person.age)
+
+        val person1 = Person("NAME", 100)
+        assertEquals(100, person1.age)
+    }
 }
