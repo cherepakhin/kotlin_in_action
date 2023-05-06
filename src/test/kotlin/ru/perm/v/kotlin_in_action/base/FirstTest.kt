03df57a4115d59ee5af23e860f4862bbb1a99135
Черепахин Василий <vasi.che@gmail.com>
2023-05-06 13:31:47 +0500

    Add comments

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/FirstTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/FirstTest.kt
@@ -8,6 +8,7 @@ class FirstTest {
 
     @Test
     internal fun create() {
+        // определение класса прямо внутри метода. Внутренний класс
         data class Person(val name: String, val age: Int)
 
         val person = Person("NAME", 100)
@@ -18,6 +19,7 @@ class FirstTest {
     @Test
     internal fun createWithNull() {
         // "?" м.б. null
+        // определение класса прямо внутри метода. Внутренний класс
         data class Person(val name: String, val age: Int?)
         // val age: Int? м.б. null. В конструкторе указано, что age нужно определить и можно null
         val person = Person("NAME", null)
@@ -27,6 +29,7 @@ class FirstTest {
 
     @Test
     internal fun createWithDefaultIfNull() {
+        // определение класса прямо внутри метода. Внутренний класс
         // age:Int? т.е. м.б. null
         data class Person(val name: String, val age: Int? = 10)
         // присваиваю null. Можно присвоить, т.к. Int?
