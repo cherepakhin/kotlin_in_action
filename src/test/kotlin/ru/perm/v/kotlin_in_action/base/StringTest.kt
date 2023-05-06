657e253b9dab2c6e8ac1239198419758596e29f3
Черепахин Василий <vasi.che@gmail.com>
2023-05-06 12:34:27 +0500

    StringTest.kt String template

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/StringTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/StringTest.kt
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test
 
 internal class StringTest {
     @Test
-    internal fun valvarsubs() {
+    internal fun simple() {
         val s1 = "s1"
         var s1a = s1 + "a"
         Assertions.assertEquals("s1a", s1a)
     }
 
+    @Test
+    internal fun strTemplate() {
+        val s = "STR"
+        val templ = "s=${s}"
+        Assertions.assertEquals("s=STR", templ)
+    }
 }
