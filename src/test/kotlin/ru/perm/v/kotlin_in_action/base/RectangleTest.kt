0189895037259f98f52a6b85281bcb34ce3821ee
Черепахин Василий <vasi.che@gmail.com>
2023-05-08 16:19:08 +0500

    demo props

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/RectangleTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/RectangleTest.kt
@@ -1,6 +1,8 @@
 package ru.perm.v.kotlin_in_action.base
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class RectangleTest {
@@ -22,6 +24,16 @@ class RectangleTest {
 
     @Test
     internal fun isNotKvadrat() {
-        Assertions.assertFalse(Rectangle(1, 5).isSquireProp)
+        // обращение к свойству isSquireProp через метод get, в отличие от прямого обращения к переменной
+        // в примере выше обращение к методу isSquire(), а тут как к свойству
+        val rect = Rectangle(1, 5)
+        // Везде обращение как к полям
+        // rect.length
+        // rect.width
+        // rect.isSquireProp
+        assertTrue(rect.length.equals(1))
+        assertTrue(rect.width.equals(5))
+        assertFalse(rect.isSquireProp)
+
     }
 }
