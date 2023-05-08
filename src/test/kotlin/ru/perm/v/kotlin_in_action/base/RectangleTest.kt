6a640fb84276e1e77d2161f929b32820a3127d01
Черепахин Василий <vasi.che@gmail.com>
2023-05-08 16:54:48 +0500

    color vaR - менять можно

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/RectangleTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/RectangleTest.kt
@@ -1,12 +1,11 @@
 package ru.perm.v.kotlin_in_action.base
 
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class RectangleTest {
-    class Rectangle(val length: Int = 0, val width: Int) {
+    class Rectangle(val length: Int = 0, val width: Int, var color: String = "") {
 
         val isSquireProp: Boolean
             get() {
@@ -19,6 +18,7 @@ class RectangleTest {
         fun isSquireFun(): Boolean {
             return length.equals(width)
         }
+
     }
 
     @Test
@@ -29,16 +29,23 @@ class RectangleTest {
     @Test
     internal fun isNotKvadrat() {
         val rect = Rectangle(1, 5)
-        // Везде обращение как к полям
-        // rect.length
-        // rect.width
-        // rect.isSquireProp
+        assertFalse(rect.isSquireFun())
         // обращение к свойству isSquireProp через метод get, в отличие от прямого обращения к переменной
         // в примере выше обращение к методу isSquireFun(), а тут как к СВОЙСТВУ val a = rect.isSquireProp
         assertFalse(rect.isSquireProp)
         assertFalse(rect.isSquireShortProp)
+        // Везде обращение как к полям
+        // rect.length
+        // rect.width
         assertTrue(rect.length.equals(1))
         assertTrue(rect.width.equals(5))
+    }
 
+    @Test
+    internal fun colorTest() {
+        val rect = Rectangle(1, 5)
+        // rect.width =10 не прокатит, т.к. vaL, а color vaR - менять можно
+        rect.color = "RED"
+        assertEquals("RED", rect.color)
     }
 }
