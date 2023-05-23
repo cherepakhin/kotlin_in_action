3aa0a0c0d438d5c5a8b83cba67e2ef6254684725
Черепахин Василий <vasi.che@gmail.com>
2023-05-23 11:12:00 +0500

    RESOLVE!!! "NO WARNINGS for generate allure report" for geberate allure report

--- src/test/java/PersonJTest.java
+++ src/test/java/PersonJTest.java
@@ -2,12 +2,18 @@ import org.junit.jupiter.api.Test;
 import ru.perm.v.kotlin_in_action.PersonK;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Использование java с kotlin
+ */
 public class PersonJTest {
     @Test
     void create() {
         PersonK person = new PersonK(1, "-", 10);
+        Integer age = person.getAge();
+
         assertEquals("-", person.getName());
-        assertEquals(10, person.getAge());
+        assertTrue(age.equals(10));
     }
 }
