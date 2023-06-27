bc6203117b48269882a0a88b62bfe7b5a2a12c0e
Черепахин Василий <vasi.che@gmail.com>
2023-06-27 11:40:55 +0500

    Использоване функции без создания класса. ЖОПА!

--- src/main/kotlin/ru/perm/v/kotlin_in_action/OpenFunc.kt
+++ src/main/kotlin/ru/perm/v/kotlin_in_action/OpenFunc.kt
@@ -2,6 +2,7 @@ package ru.perm.v.kotlin_in_action
 
 val aConst = ", CONST"
 
+// В Kotlin можно определить только функцию, БЕЗ СОЗДАНИЯ КЛАССА
 fun getCalcFunc(): String {
-    return "id.toString() + name + age"+ aConst
+    return "id.toString() + name + age" + aConst
 }
