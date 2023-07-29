fddc0c15334cccf24b7222b158e4d7882058d03c
Черепахин Василий <vasi.che@gmail.com>
2023-07-29 16:20:21 +0500

    add MapTest.kt

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/MapTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/MapTest.kt
@@ -8,11 +8,17 @@ class MapTest {
     @Test
     internal fun simple() {
         val mutableMap = mutableMapOf<Int, PersonK>()
-        val person10 = PersonK(10, "name10", 11)
+        // also - еще один прикол kotlin
+        val person10 = PersonK(10, "name10", null).also { it.age = 10 }
         val person20 = PersonK(20, "name20", 21)
         mutableMap.put(person10.id, person10)
         mutableMap.put(person20.id, person20)
+
         Assertions.assertEquals(2, mutableMap.size)
         Assertions.assertEquals(person10, mutableMap.get(10))
+        Assertions.assertTrue(mutableMap.containsValue(person10))
+
+        val person30 = PersonK(30, "name30", 31)
+        Assertions.assertFalse(mutableMap.values.contains(person30))
     }
 }
