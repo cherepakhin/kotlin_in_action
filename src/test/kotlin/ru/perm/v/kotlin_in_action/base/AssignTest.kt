4efcc8f93b2dab48793bdf7f09c97d9652ec22b4
Черепахин Василий <vasi.che@gmail.com>
2023-07-29 13:25:22 +0500

    AssignTest.kt // Обычное, типичное определение класса. Не укороченное как var a.

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/AssignTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/AssignTest.kt
@@ -12,7 +12,7 @@ class AssignTest {
         set(value) { // сама переменная д.б. VAR! (var a). C VAL не прокатит (константа и т.п.)
             // сеттера НЕТ. вместо вызова сеттера, НУЖНО использовать '='
             println(String.format("SETTING NEW VAL for a: %s", value))
-            // тут "field" ключевое слово, доступ к значению
+            // тут "field" ключевое слово, доступ к значению именно "a"
             field = value
         }
 
@@ -23,18 +23,18 @@ class AssignTest {
         Assertions.assertEquals("-", a)
     }
 
-    class B {
-        private var field1 =0
-        fun setField1(newVal:Int) {
-            field1 = newVal
-        }
-        fun getField1() : Int {
-            return  field1
-        }
-    }
-
     @Test
     internal fun testClassB() {
+        // Обычное, типичное определение класса. Не укороченное как var a.
+        class B {
+            private var field1 =0
+            fun setField1(newVal:Int) {
+                field1 = newVal
+            }
+            fun getField1() : Int {
+                return  field1
+            }
+        }
         val b = B()
         b.setField1(2)
         assertEquals(2, b.getField1())
