39aac82c96484e0e9b4ea3400b2be5fd0cdab231
Черепахин Василий <vasi.che@gmail.com>
2023-08-18 13:12:50 +0500

    muttable, unmuttable for list

--- src/test/kotlin/ru/perm/v/kotlin_in_action/base/VarArgTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/base/VarArgTest.kt
@@ -17,21 +17,43 @@ class VarArgTest {
     }
 
 
-    class AboutVarArg(
-        var size: Int = 0,
-        var stringConevrtedFromArr: String = ""
-    ) {
+    class AboutVarArgAndMuttable() {
+        var size: Int = 0
+//        var stringConevrtedFromVarArg: String = ""
+        var stringConevrtedFromVarArg: String = ""
+        var strList = listOf<String>("") // UNMUTTABLE
+        val strListMuttable = mutableListOf<String>()
+
         constructor(vararg strings: String) : this() {
-            size = strings.size
-            stringConevrtedFromArr = strings.fold("") { acc, s -> "$acc$s" }
+            size = strings.size // как у массива
+            strList = strings.asList()
+
+            stringConevrtedFromVarArg = strings.fold("") { acc, s -> "$acc$s" }
+
+// Присвоить другой лист можно, т.к. var
+//            strList = listOf()
+// ERROR!!! listOf<String>("") is UNMUTTABLE. Добавить, НЕЛЬЗЯ для add() нужно сконвертировать .toMutableList()
+//            strList.add("")
+
+//  для add() нужно сконвертировать .toMutableList()
+            val strListMuttable = strings.toMutableList() // вместо strList = strings.asList()
+            strListMuttable.add("AAA")
+// так не прокатывает java.lang.UnsupportedOperationException
+//            var strListMuttable3 = (strList as MutableList<String>).add("AAA")
         }
-
     }
-
 
     @Test
     fun aboutVarArg() {
-        val aboutVarArg = AboutVarArg("var1", "var2")
-        Assertions.assertEquals("var1var2", aboutVarArg.stringConevrtedFromArr)
+        // тут в конструкторе 2 параметра
+        val aboutVarArg2 = AboutVarArgAndMuttable("var1", "var2")
+        Assertions.assertEquals("var1var2", aboutVarArg2.stringConevrtedFromVarArg)
+        Assertions.assertEquals(2, aboutVarArg2.size)
+
+        // тут уже 3 параметра, а класс ТОТ ЖЕ AboutVarArg
+        val aboutVarArg3 = AboutVarArgAndMuttable("var1", "var2", "var3")
+        Assertions.assertEquals("var1var2var3", aboutVarArg3.stringConevrtedFromVarArg)
+        Assertions.assertEquals(3, aboutVarArg3.size)
+
     }
 }
