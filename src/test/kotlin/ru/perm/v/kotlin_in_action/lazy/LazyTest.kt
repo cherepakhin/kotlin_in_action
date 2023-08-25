eb47fe2c222f3267835ece609e4e3156c90b73d1
Черепахин Василий <vasi.che@gmail.com>
2023-08-25 12:14:01 +0500

    add LazyTest.kt

--- src/test/kotlin/ru/perm/v/kotlin_in_action/lazy/LazyTest.kt
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/lazy/LazyTest.kt
@@ -2,6 +2,7 @@ package ru.perm.v.kotlin_in_action.lazy
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.lang.String.format
 import java.util.concurrent.atomic.AtomicInteger
 
 class LazyTest {
@@ -9,15 +10,24 @@ class LazyTest {
     fun givenLazyValue_whenGetIt_thenShouldInitializeItOnlyOnce() {
         // given
         val numberOfInitializations: AtomicInteger = AtomicInteger()
+
+        // обычная инициализация:
+        // val ss = ClassWithHeavyInitialization()
+
+//        println(numberOfInitializations)
+        // lazy инициализация:
         val lazyValue: ClassWithHeavyInitialization by lazy {
             numberOfInitializations.incrementAndGet()
             ClassWithHeavyInitialization()
         }
         // when
-        println(numberOfInitializations)
-        println(lazyValue)
-        println(numberOfInitializations)
-        println(lazyValue)
+        println(format("start numberOfInitializations: %s",numberOfInitializations.get()))
+
+        println(format("lazyValue: %s",lazyValue))
+        println(format("after get first lazyValue, numberOfInitializations: %s",numberOfInitializations.get()))
+
+        println(format("lazyValue: %s",lazyValue))
+        println(format("after second get lazyValue, numberOfInitializations: %s",numberOfInitializations.get()))
 
         // then
         assertEquals(1, numberOfInitializations.get())
