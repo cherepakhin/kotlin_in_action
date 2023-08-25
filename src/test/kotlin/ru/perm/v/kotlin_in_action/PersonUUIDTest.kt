04844403e1757c7b3cd6b3a42a6d05ff0219eebb
Черепахин Василий <vasi.che@gmail.com>
2023-08-25 10:59:11 +0500

    add PersonUUIDTest

---
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/PersonUUIDTest.kt
@@ -0,0 +1,26 @@
+package ru.perm.v.kotlin_in_action
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PersonUUIDTest {
+
+    @Test
+    fun generateNew() {
+        val person = PersonUUID("NAME")
+        println(person.id)
+        assertEquals("NAME", person.name)
+    }
+
+    @Test
+    fun compareGenerated_2_Person() {
+        val person1 = PersonUUID("NAME")
+        val person2 = PersonUUID("NAME")
+        println(person1.id)
+        println(person2.id)
+        assertNotEquals(person1.id,person2.id)
+    }
+
+
+
+}
