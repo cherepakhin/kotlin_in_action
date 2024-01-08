c9c09dfae223e3e67ae96f5717c7e6b403a86191
Черепахин Василий <vasi.che@gmail.com>
2024-01-08 23:42:40 +0500

    add class SealedTest

---
+++ src/test/kotlin/ru/perm/v/kotlin_in_action/sealed/SealedTest.kt
@@ -0,0 +1,42 @@
+package ru.perm.v.kotlin_in_action.sealed
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SealedTest {
+    sealed class MyResult {
+        data class Success(val data: String) : MyResult()
+        data class Error(val message: String) : MyResult()
+        object Loading : MyResult()
+    }
+
+    fun getResult(str: String = ""): MyResult {
+        if (str == "DATA") return MyResult.Success(str)
+        if (str == "ERROR") return MyResult.Error(str)
+        return MyResult.Loading
+    }
+
+    @Test
+    fun successTest() {
+        val result = getResult("DATA")
+        assertEquals(MyResult.Success("DATA"), result)
+//        when (result) {
+//            is MyResult.Success -> assertEquals(MyResult.Success("DATA"), result)
+//            is MyResult.Error -> println("Error: ${result.message}")
+//            MyResult.Loading -> println("Loading...")
+//        }
+    }
+
+    @Test
+    fun errorTest() {
+        val result = getResult("ERROR")
+        assertEquals(MyResult.Error("ERROR"), result)
+    }
+
+    @Test
+    fun loadingTest() {
+        val result = getResult("")
+        assertEquals(MyResult.Loading, result)
+    }
+
+}
