2825a6bd75042ae2d4fb839dac58a2f3d3449d9e
Черепахин Василий <vasi.che@gmail.com>
2024-07-16 17:21:33 +0500

    add comments ImmutableTest.kt

--- src/test/kotlin/ImmutableTest.kt
+++ src/test/kotlin/ImmutableTest.kt
@@ -8,23 +8,24 @@ class ImmutableTest {
     fun compareListStrings() {
         val immutableList = listOf("one", "two")
         var s = immutableList[0] // строки копируются
-        s = "S"
+        s = "NEW_VAL"
         assertEquals(listOf("one", "two"), immutableList)
+        // т.к. строки копируются, immutableList[0] остался прежним (не "NEW_VAL")
     }
 
     @Test
     fun compareListObjects() {
-        val o1 = O()
-        o1.setProp("o")
-        val o2 = O()
-        o2.setProp("o")
+        val prevO1 = O()
+        prevO1.setProp("o")
+        val prevO2 = O()
+        prevO2.setProp("o")
 
-        assertEquals(o1, o2)
+        assertEquals(prevO1, prevO2)
 
-        val list1 = listOf<O>(o1, o1)
-        val list2 = listOf<O>(o2, o2)
+        val list1 = listOf<O>(prevO1, prevO1)
+        val list2 = listOf<O>(prevO2, prevO2)
 
-        // EQUALS! Сравниваются значения в list, несмотря на то , что ОЪЕКТЫ(адреса) list разные!
+        // EQUALS! Сравниваются значения в list, несмотря на то , что адреса ОЪЕКТОВ в list разные!
         // и экземпляры объектов разные (но у них одинаковые значения)
         // т.о. List-ы сравниваются правильно и тщательно
         // Переопределение equals, hashCode в O.kt обязательно!!!
